a417fa44d19013df32f7f20667ed9813fc5372b7
natlwp <b.natalia@escolar.ifrn.edu.br>
2026-09-10 10:28:49 -0300

    Primeiro commit do projeto

---
+++ todolist - MVC/app/models/tarefa.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class Tarefas{
+    private $conn;
+    public function __construct(){
+        $db = new Database();
+        $this->conn = $db->conectar();
+    }
+};
+
+?>
